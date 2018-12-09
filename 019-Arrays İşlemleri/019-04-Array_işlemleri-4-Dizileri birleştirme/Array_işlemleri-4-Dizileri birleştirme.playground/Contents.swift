01f4a26f89bbf6916c3715541854661dd18c2bf9
aydincan77 <aydincan77@gmail.com>
2018-12-09 15:32:04 +0300

    günceleme swift 4.2

--- 019-Arrays İşlemleri/019-04-Array_işlemleri-4-Dizileri birleştirme/Array_işlemleri-4-Dizileri birleştirme.playground/Contents.swift
+++ 019-Arrays İşlemleri/019-04-Array_işlemleri-4-Dizileri birleştirme/Array_işlemleri-4-Dizileri birleştirme.playground/Contents.swift
@@ -2,28 +2,29 @@ import UIKit
 
 // COMBİNİNG ARRAYS : DİZİLERİ BİRLEŞTİRME
 
-var sayıDizisiBirleştirme = [1, 2, 3, 4, 5]
-
 //append(contentsOf:) : Dizi koleksiyonunun en sonuna bir sayı aralığı ekleme
-sayıDizisiBirleştirme.append(contentsOf: 10...15)
-print(sayıDizisiBirleştirme)
+var sayıDizisiBirleştirme1 = [1, 2, 3, 4, 5]
+
+sayıDizisiBirleştirme1.append(contentsOf: 6...10)
 
 // +(_:_:) : Bir dizinin ve koleksiyonun öğelerini birleştirerek yeni bir koleksiyon oluşturur.
-var sayıAralığı1 = 6...9
-var sayılarıEkle1 = sayıAralığı1 + sayıDizisiBirleştirme
-print(sayılarıEkle1)
+var sayıDizisiBirleştirme2 = [6, 7, 8, 9, 10]
+var sayıAralığı2 = 1...5
+var sonuç2 = sayıAralığı2 + sayıDizisiBirleştirme2
 
-var sayıAralığı2 = 6...9
-var sayılarıEkle2 = sayıDizisiBirleştirme + sayıAralığı2
-print(sayılarıEkle2)
+var sayıDizisiBirleştirme3 = [1, 2, 3, 4, 5]
+var sayıAralığı3 = 6...10
+var sonuç3 = sayıDizisiBirleştirme3 + sayıAralığı3
+
+var sayıDizisiBirleştirme4 = [1, 2, 3, 4, 5]
+var sayıAralığı4 = 6..<10
+var sonuç4 = sayıDizisiBirleştirme4 + sayıAralığı4
 
 var dahaKüçükSayılar = [1, 2, 3, 4]
 var dahaBüyükSayılar = [5, 6, 7, 8, 9, 10]
-var tümSayılar = dahaKüçükSayılar + dahaBüyükSayılar
-print(tümSayılar)
+var sonuç = dahaKüçükSayılar + dahaBüyükSayılar
 
 // +=(_:_:) : Bir dizinin öğelerini aralık değiştirilebilen bir koleksiyona ekler.
 var mevcutSayıDizisi = [1, 2, 3, 4, 5]
 mevcutSayıDizisi += 10...15
-print(mevcutSayıDizisi)
 
