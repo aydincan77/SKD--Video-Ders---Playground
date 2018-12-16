cfc4cdd36dc907af755f4d10cd8d7ce4e5380ed2
aydincan77 <aydincan77@gmail.com>
2018-12-16 16:16:24 +0300

    güncelleme Swift 4.2

--- 019-Arrays İşlemleri/019-04-Array_işlemleri-4-Dizileri birleştirme/Array_işlemleri-4-Dizileri birleştirme.playground/Contents.swift
+++ 019-Arrays İşlemleri/019-04-Array_işlemleri-4-Dizileri birleştirme/Array_işlemleri-4-Dizileri birleştirme.playground/Contents.swift
@@ -7,7 +7,7 @@ var sayıDizisiBirleştirme1 = [1, 2, 3, 4, 5]
 
 sayıDizisiBirleştirme1.append(contentsOf: 6...10)
 
-// +(_:_:) : Bir dizinin ve koleksiyonun öğelerini birleştirerek yeni bir koleksiyon oluşturur.
+// +(_:_:) : Bir diziye belirteceğiniz yeni değer veya değerleri ekleyerek yeni bir koleksiyon oluşturur.
 var sayıDizisiBirleştirme2 = [6, 7, 8, 9, 10]
 var sayıAralığı2 = 1...5
 var sonuç2 = sayıAralığı2 + sayıDizisiBirleştirme2
@@ -24,7 +24,7 @@ var dahaKüçükSayılar = [1, 2, 3, 4]
 var dahaBüyükSayılar = [5, 6, 7, 8, 9, 10]
 var sonuç = dahaKüçükSayılar + dahaBüyükSayılar
 
-// +=(_:_:) : Bir dizinin öğelerini aralık değiştirilebilen bir koleksiyona ekler.
+// +=(_:_:) : Bir diziye belirteceğiniz bir aralık dilimi veya değer ekler.
 var mevcutSayıDizisi = [1, 2, 3, 4, 5]
 mevcutSayıDizisi += 10...15
 
