1127e5fb2860778817838443766e72ec3ac48cd9
aydincan77 <aydincan77@gmail.com>
2018-12-16 20:18:57 +0300

    güncelleme Swift 4.2

--- 019-Arrays İşlemleri/019-05-Array_işlemleri-5-Elementleri kaldırma/Array_işlemleri-5-Elementleri kaldırma.playground/Contents.swift
+++ 019-Arrays İşlemleri/019-05-Array_işlemleri-5-Elementleri kaldırma/Array_işlemleri-5-Elementleri kaldırma.playground/Contents.swift
@@ -3,45 +3,44 @@ import UIKit
 
 // REMOVİNG ELEMENTS : ELEMENTLERİ KALDIRMA
 
-// remove(at:) : Elementi, belirtilen konumdan kaldırır ve geri döndürür.
+// remove(at:) : Index değeri belirtilen Elementi bize gösterir ve kaldırır. Sonuç olarak element değeri döndürür.
 var doubleDeğerler = [1.1, 1.5, 2.9, 1.2, 1.5, 1.3, 1.2]
 let değeriKaldır = doubleDeğerler.remove(at: 2)
 print(doubleDeğerler)
 
-// removeFirst() : Dizi koleksiyonunun ilk elementini kaldır
+// removeFirst() : Dizi'nin ilk elementini bize gösterir ve kaldırır. Sonuç olarak element değeri döndürür.
 var hayvanlarDizisi = ["Karınca", "Örümcek", "Yılan", "Arı", "Kaplumbağa", "Kedi", "Köpek", "Bülbül"]
 hayvanlarDizisi.removeFirst()
 print(hayvanlarDizisi)
 
-// removeFirst(_:) : Dizi koleksiyonundan belirtilen sayı kadar elementi koleksiyonun başlangıcından kaldırır.
+// removeFirst(_:) : Dizi'nin başından başlayarak belirtilen index değerine kadar olan elementi diziden kaldırır. Sonuç olarak element değeri döndürür.
 hayvanlarDizisi.removeFirst(3)
 print(hayvanlarDizisi)
 
-// removeLast() : Dizi koleksiyonunun son elementini kaldır
+// removeLast() : Dizi'nin son elementini bize gösterir ve kaldırır. Sonuç olarak element değeri döndürür.
 hayvanlarDizisi.removeLast()
 print(hayvanlarDizisi)
 
-// removeLast (_:) : Dizi koleksiyonundan belirtilen sayı kadar elementi koleksiyonun başlangıcından kaldırır.
+// removeLast (_:) : Dizi'nin sonundan başlayarak belirtilen index değerine kadar olan elementi diziden kaldırır. Sonuç olarak element değeri döndürür.
 hayvanlarDizisi.removeLast(2)
 print(hayvanlarDizisi)
 
-// removeSubrange(_:) : Dizi koleksiyonunun belirtilen aralıklardaki elementlerini koleksiyondan kaldırır.
-var değerlerim = [1.2, 1.5, 2.9, 2.0, 3.5]
-değerlerim.removeSubrange(0..<3)
+// removeSubrange(_:) : Dizi'den kaldırılması için belirtiğimiz index değerlerinin elementlerini kaldırır ve bize geriye kalan elementleri gösterir. Sonuç olarak element değeri döndürür.
+var değerlerim = [1.2, 3.5, 2.5, 2.2, 1.5, 4.1]
+değerlerim.removeSubrange(1...3)
 print(değerlerim)
 
-// removeAll() : Dizi koleksiyonun tüm elementlerini kaldır
+// removeAll() : Dizi'nin tüm elementlerini kaldır. Sonuç olarak element değeri döndürür.
 var diziDeğerleri1 = [5, 6, 7, 8, 9, 10, 11]
 diziDeğerleri1.removeAll()
 print(diziDeğerleri1)
 
-// removeAll(where:) : Verilen yüklemeyi karşılayan tüm öğeleri kaldırır.
+// removeAll(where:) : Verilen yüklemeyi karşılayan tüm öğeleri kaldırır. Sonuç olarak Bool bir değer döndürür, true dönenleri kaldırır, false dönenleri dizi'de tutar
 var diziDeğerleri2 = [5, 6, 7, 8, 9, 10, 11]
 diziDeğerleri2.removeAll(where: { $0 % 2 == 1 })
 print(diziDeğerleri2)
 
-// popLast() : Dizi koleksiyonunun son elementini kaldırır ve geri gönderir.
+// popLast() : Dizi'nin son elementini gösterir ve kaldırır. Sonuç olarak element değeri döndürür.
 var diziDeğerleri3 = [5, 6, 7, 8, 9, 10, 11]
 diziDeğerleri3.popLast()
 print(diziDeğerleri3)
-
