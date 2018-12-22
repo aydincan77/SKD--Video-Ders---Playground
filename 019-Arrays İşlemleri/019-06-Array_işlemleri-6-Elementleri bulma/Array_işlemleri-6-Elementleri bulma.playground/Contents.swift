798733d0c82390553d8a38934904aeb439befac2
aydincan77 <aydincan77@gmail.com>
2018-12-22 16:44:37 +0300

    güncelleme swift 4.2

--- 019-Arrays İşlemleri/019-06-Array_işlemleri-6-Elementleri bulma/Array_işlemleri-6-Elementleri bulma.playground/Contents.swift
+++ 019-Arrays İşlemleri/019-06-Array_işlemleri-6-Elementleri bulma/Array_işlemleri-6-Elementleri bulma.playground/Contents.swift
@@ -4,28 +4,28 @@ import UIKit
 
 // FİNDİNG ELEMENTS - ELEMENTLERİ BULMA
 
-// contains : Dizinin verilen elementi içerip içermediğini belirten bir Boole değeri döndürür.
-let oyuncular = ["Vivien", "Marlon", "Kim", "Karl"]
-print(oyuncular.contains("Marlon"))
-print(oyuncular.contains("James"))
+// contains(<#T##element: String##String#>) : Dizinin verilen elementi içerip içermediğini belirten bir Boole değeri döndürür.
+let oyuncular = ["Haluk", "Emre", "Mert", "Kemal", "Şener"]
+oyuncular.contains("Mert")
+oyuncular.contains("Kenan")
 
-// contains(where:) : Dizinin verilen yüklemeyi karşılayan bir öğe içerip içermediğini belirten bir Boole değeri döndürür.
+// contains(where: <#T##(Double) throws -> Bool#>) : Dizinin verilen yüklemeyi karşılayan bir öğe içerip içermediğini belirten bir Boole değeri döndürür.
 let satışlar = [21.37, 55.21, 9.32, 10.18, 388.77, 11.41]
 let büyükAlımlar = satışlar.contains { $0 > 100 }
 print(büyükAlımlar)
 
-// allSatisfy(_:) : Bir dizinin her bir elementini belirli bir yüklemi karşılayıp karşılamadığını gösteren bir Boole değeri döndürür.
-let isimler = ["Sofia", "Camilla", "Martina", "Mateo", "Nicolás"]
+// allSatisfy(<#T##predicate: (String) throws -> Bool##(String) throws -> Bool#>) : Bir dizinin her bir elementini belirli bir yüklemi karşılayıp karşılamadığını gösteren bir Boole değeri döndürür.
+let isimler = ["Tarık", "Cüneyt", "Mustafa", "Elif", "Sena"]
 let enAzbeşHarfOlanlar = isimler.allSatisfy({ $0.count >= 5 })
 print(enAzbeşHarfOlanlar)
 
-// first(where:) :  Verilen yüklemeyi karşılayan dizinin ilk öğesini döndürür.
+// first(where: <#T##(Int) throws -> Bool#>) :  Verilen yüklemeyi karşılayan dizinin ilk öğesini döndürür.
 let ilkSayıDeğeriniBul = [3, 7, 4, -2, 9, -1, 10, 1]
 if let ilkNegatifSayı = ilkSayıDeğeriniBul.first(where: { $0 < 0 }) {
     print("Dizinin ilk negatif sayısı \(ilkNegatifSayı)'dır.")
 }
 
-// firstIndex(of:) : Aranan değerin koleksiyonda göründüğü ilk indeks değerini döndürür.
+// firstIndex(of: <#T##String#>) : Aranan değerin koleksiyonda göründüğü ilk indeks değerini döndürür.
 var öğrenciler = ["Ben", "Ivy", "Jordell", "Maxime", "Kofi", "Abena", "Peter", "Kweku", "Akosua"]
 öğrenciler.firstIndex(of: "Maxime")
 
@@ -34,18 +34,18 @@ if let i = öğrenciler.firstIndex(of: "Maxime") {
 }
 print(öğrenciler)
 
-// firstIndex(where:) : Dizi Koleksiyonunun belirtilen yüklemeyi karşıladığı ilk indeks değerini döndürür.
+// firstIndex(where: <#T##(String) throws -> Bool#>) : Dizi Koleksiyonunun belirtilen yüklemeyi karşıladığı ilk indeks değerini döndürür.
 if let i = öğrenciler.firstIndex(where: { $0.hasPrefix("K") }) {
     print("Aranan değer ile başlayan ilk element \(öğrenciler[i])'dır.")
 }
 
-// last(where:) : Verilen yüklemeyi karşılayan dizinin son elementini döndürür.
+// last(where: <#T##(Int) throws -> Bool#>) : Verilen yüklemeyi karşılayan dizinin son elementini döndürür.
 let sonSayıDeğeriniBul = [3, 7, 4, -2, 9, -6, 10, 1]
 if let sonNegatif = sonSayıDeğeriniBul.last(where: { $0 < 0 }) {
     print("Dizinin son negatif sayısı \(sonNegatif)'dır.")
 }
 
-// lastIndex(of:) : Belirtilen değerin koleksiyonda göründüğü son değerin indeks değerini döndürür.
+// lastIndex(of: <#T##String#>) : Belirtilen değerin koleksiyonda göründüğü son değerin indeks değerini döndürür.
 var sonİsimDizisi = ["Ben", "Ivy", "Jordell", "Ben", "Maxime"]
 sonİsimDizisi.lastIndex(of: "Ben")
 
@@ -54,7 +54,7 @@ if let i = sonİsimDizisi.lastIndex(of: "Ben") {
 }
 print(sonİsimDizisi)
 
-// lastIndex(where:) : Belirtilen yüklemeyle eşleşen koleksiyondaki son öğenin dizinini döndürür.
+// lastIndex(where: <#T##(String) throws -> Bool#>) : Belirtilen yüklemeyle eşleşen koleksiyondaki son öğenin dizinini döndürür.
 let ileBitenSonDeğer = ["Kofi", "Abena", "Peter", "Kweku", "Akosua"]
 if let i = ileBitenSonDeğer.lastIndex(where: { $0.hasPrefix("A") }) {
     print("Aranan değer ile biten son element \(ileBitenSonDeğer[i])'dir.")
